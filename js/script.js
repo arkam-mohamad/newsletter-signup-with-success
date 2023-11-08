be04b2dca61963ef930d9a41e7d32d6af6a0dc81
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2023-11-08 14:11:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+$('#subscribe').click(function (e) { 
+  if(isEmail($('input').val()) == false){
+    $('input').addClass('red');
+    $('.error').removeClass('hidden');
+  } else {
+    $('input').removeClass('red');
+    $('.error').addClass('hidden');
 
+    $('main').addClass('hidden');
+    $('aside').removeClass('hidden')
+  }
+});
+$('#dismiss').click(function (e) { 
+  $('aside').addClass('hidden');
+  $('main').removeClass('hidden')
+});
+
+
+function isEmail(mail){
+  let regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  if(!regex.test(mail)){
+    return false
+  } else {
+    return true
+  }
+}
